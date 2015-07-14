df51cf0abe10ab4228380ed4ed2b3c04b3a6f83a
Yaozong Zhu <yaozong.zhu@fmr.com>
2015-07-14 16:22:36 +0100

    remove copyright comment

--- types/soap.go
+++ types/soap.go
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2015 XTRAC LLC. All Rights Reserved.
- * Fidelity Confidential Information
- *
- *This software and all information contained herein is the property
- *of Fidelity Investments.  Much of this information including ideas,
- *concepts, formulas, processes, data, know-how, techniques, and
- *the like, found herein is considered proprietary to Fidelity Investments,
- *and may be covered by U.S. and foreign patents or patents pending,
- *or protected under trade secret laws.  Any dissemination, disclosure,
- *use, or reproduction of this material for any reason inconsistent with
- *the express purpose for which it has been disclosed is strictly
- *forbidden.
- *			  Restricted Rights Legend
- *			  ------------------------
- *Use, duplication, or disclosure by the Government is subject to
- *restrictions as set forth in paragraph (b)(3)(B) of the Rights in
- *Technical Data and Computer Software clause in DAR 7-104.9(a).
- *
- *Fidelity Confidential Information
- */
 package types
 
 import (
